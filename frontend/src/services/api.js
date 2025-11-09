322759616ffbd4ad65062ed8319f5db6414a734e
Aqil Wafi <m.aqil.wafi@gmail.com>
2025-11-09 22:54:50 +0700

    cek 8

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 // Gunakan environment variable agar fleksibel antara dev & prod
-const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_BASE_URL = import.meta.env.VITE_API_URL || "https://bnbe-production.up.railway.app";
 
 if (!API_BASE_URL) {
   console.error("⚠️ VITE_API_URL is undefined! Check your .env file.");
 }
 
 const api = axios.create({
-  baseURL: `/api`,
+  baseURL: `${API_BASE_URL}/api`,
   headers: { "Content-Type": "application/json" },
 });
 
