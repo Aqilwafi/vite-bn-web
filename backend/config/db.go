f285fe44cc60ade82d314f5babd428b2ba1b1bfe
Aqil Wafi <m.aqil.wafi@gmail.com>
2025-11-09 20:48:24 +0700

    update DB config fic conn

--- backend/config/db.go
+++ backend/config/db.go
@@ -15,20 +15,17 @@ func ConnectDB() (*sql.DB, error) {
 	if os.Getenv("DATABASE_URL") != "" {
 		dsn = os.Getenv("DATABASE_URL")
 
-		// FIX: ubah prefix agar lib/pq bisa baca
-		if strings.HasPrefix(dsn, "postgresql://") {
-			dsn = strings.Replace(dsn, "postgresql://", "postgres://", 1)
-		}
+		
 
 		fmt.Println("🔗 Using DATABASE_URL from environment")
 	} else {
 		dsn = fmt.Sprintf(
 			"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-			getEnv("DB_HOST", "localhost"),
-			getEnv("DB_PORT", "5432"),
-			getEnv("DB_USER", "baca_user"),
-			getEnv("DB_PASSWORD", "baca_pass"),
-			getEnv("DB_NAME", "baca_db"),
+			getEnv("PGHOST", "localhost"),
+			getEnv("PGPORT", "5432"),
+			getEnv("PGUSER", "baca_user"),
+			getEnv("PGPASSWORD", "baca_pass"),
+			getEnv("PGDATABASE", "baca_db"),
 		)
 		fmt.Println("🧩 Using local DB configuration")
 	}
